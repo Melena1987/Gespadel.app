b28692893910c96fa3a122ad1892916d334ac14b
Manu <manudesignsforyou@gmail.com>
2025-09-08 18:12:27 +0200

    fix: Update Euro icon SVG path
    The Euro icon's SVG path data was outdated and has been updated to a more current and potentially cleaner representation.

--- components/icons/CurrencyEuroIcon.tsx
+++ components/icons/CurrencyEuroIcon.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 export const CurrencyEuroIcon: React.FC = () => (
     <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-        <path strokeLinecap="round" strokeLinejoin="round" d="M17.303 7.697C16.527 6.45 15.345 5.5 14 5.5c-1.857 0-3.44 1.28-3.86 3H5m11 7c-1.345 0-2.527-.95-3.303-2.197m-2.394-2.606C9.13 11.28 7.547 10 5.69 10H5" />
+        <path strokeLinecap="round" strokeLinejoin="round" d="M14.25 7.756a4.5 4.5 0 100 8.488M7.5 10.5h5.25m-5.25 3h5.25" />
     </svg>
 );
